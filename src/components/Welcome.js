1ef4f399cb831fcda09e4702f539df522dd15967
Harpa Hreinsdottir <harpah16@ru.is>
2018-11-05 15:43:26 +0000

    added front page

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -5,48 +5,67 @@ import {
   TouchableOpacity,
   StyleSheet,
   Animated,
-  Dimensions
+  Dimensions,
+  ImageBackground,
+  Image,
 } from "react-native";
 import ChoosePlayer from "./ChoosePlayer";
+import colors from "../../colors";
 
 class Welcome extends Component {
   render() {
     const { changeYes } = this.props;
-    return (
-      <View style={styles.container}>
-        <Text style={styles.text}>Do you want to have some fun?</Text>
-        <TouchableOpacity style={styles.button} onPress={() => changeYes()}>
-          <Text>YES</Text>
-        </TouchableOpacity>
-      </View>
-    );
+    return <View style={styles.container}>
+        <ImageBackground style={styles.imageBackground} source={require("/Users/harpahreinsdottir/Documents/HR-haust-2018/ThrounSmaforrita/bigAssignment/whatdoyouknow/assets/RyanBabe.png")} style={{ width: "100%", height: "100%" }}>
+          <View style={styles.textView}>
+            <Text style={styles.text}>Do you want to have some fun?</Text>
+          </View>
+          <TouchableOpacity style={styles.button} onPress={() => changeYes()}>
+            <Text style={styles.textInButton}>YES</Text>
+          </TouchableOpacity>
+        </ImageBackground>
+      </View>;
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: colors.mediumBlue,
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
+    paddingTop:40,
   },
   text: {
-    fontSize: 26,
-    fontWeight: "bold",
-    padding: 8,
-    textShadowColor: "rgba(0, 0, 0, 0.35)",
-    textShadowOffset: { width: -1, height: 1 },
+    fontSize: 27,
+    padding:20,
     textShadowRadius: 10,
-    marginBottom: 15
+    marginBottom: 15,
+    color: colors.mediumBlue,
   },
+  
   button: {
-    backgroundColor: "#4BB543",
+    backgroundColor: colors.mediumBlue,
     paddingBottom: 12,
     paddingTop: 12,
     paddingLeft: 25,
     paddingRight: 25,
     borderWidth: 1,
-    borderRadius: 8
+    borderRadius: 8,
+    marginLeft: 120,
+    marginRight: 120,
+    marginTop:15,
+    alignItems:"center",
+  },
+  textView:{
+    marginTop:650,
+    backgroundColor: "white",
+    alignItems:"center",
+  },
+  textInButton:{
+    color: "white",
+    fontWeight: "bold",
+    fontSize:20,
   }
 });
 
